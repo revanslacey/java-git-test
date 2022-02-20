fef92171ca4cbb35025b42b7dee5ab8299c9c4c3
Richard <richard@psychicplumbing.com>
2022-02-20 13:17:10 +0000

    Merge branch and add line

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("This is going in another direction");
         System.out.println("More on this branch");
         System.out.println("And more again");
+        System.out.println("This should now be in 'master'");
     }
 }
