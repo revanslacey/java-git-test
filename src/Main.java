43b2b384cffa57007f5e41b5d0bb8d2cb752fc05
Richard <richard@psychicplumbing.com>
2022-02-20 13:00:19 +0000

    New branch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("This is the first bit of my program in main");
+        System.out.println("This is going in another direction");
     }
 }
